382ba52a7aa080737c266cd7ddd5102f25f8cb35
Alessandro Amato del Monte <alessandro.adm@gmail.com>
2021-04-22 13:01:28 +0200

    add t2021 notebook

--- avo_class_4_investigation_funcs.py
+++ avo_class_4_investigation_funcs.py
@@ -1,6 +1,42 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def percdiff(start, end):
+    return (end-start)/start
+
+
+def find_nearest(a, a0):
+    '''
+    Element in nd array `a` closest to the scalar value `a0`
+    '''
+    idx = np.abs(a - a0).argmin()
+    return idx, a[idx]
+
+
+def bulk(vp, vs, rho):
+    '''
+    Calculate bulk modulus.
+    (aadm 2017)
+
+    Parameters
+    ----------
+    vp, vs: float
+        P- and S-wave velocity [m/s].
+    rho: float
+        Density [g/cm3].
+
+    Returns
+    -------
+    K: float
+        Bulk modulus [GPa].
+    '''
+    # converts density to SI (kg/m3)
+    D = rho*1e3
+    K = D*vp**2 - 4/3*D*vs**2
+    return K/1e9
+
 
 def gassmann(vp1, vs1, rho1, rhof1, kfluid1, rhof2, kfluid2, kmin, phi):
     '''
@@ -56,28 +92,140 @@ def gassmann(vp1, vs1, rho1, rhof1, kfluid1, rhof2, kfluid2, kmin, phi):
         vs2 = np.sqrt(mu2 / d2)
         return vp2, vs2, d2/1e3, k2/1e9, kd/1e9
 
-    
-def bulk(vp, vs, rho):
+
+def gassmann_phi(ksat1, ksat2, kf1, kf2, kmin):
+    a = (kmin-ksat1)*(kmin-ksat2)*(kf1-kf2) 
+    b = (kmin-kf1)*(kmin-kf2)*(ksat1-ksat2)
+    return a / b
+
+
+def hertzmindlin(K0, G0, sigma, phi_c=0.4, Cn=8.6, f=1):
     '''
-    Calculate bulk modulus.
-    (aadm 2017)
+    Hertz-Mindlin model.
+    (aadm 2015)
 
     Parameters
-    ----------
-    vp, vs: float
-        P- and S-wave velocity [m/s].
-    rho: float
-        Density [g/cm3].
+    ----------   
+    K0, G0 : float or array_like
+        Mineral bulk & shear modulus in GPa.
+    phi : float or array_like
+        Porosity.
+    sigma : float
+        Effective stress in MPa.
+    phi_c : float, optional
+        Critical porosity. Default: 0.4
+    Cn : float, optional
+        Coordination number Default: 8.6.
+    f : float, optional
+        Shear modulus correction factor,
+        f=1 for dry pack with perfect adhesion
+        between particles and f=0 for dry frictionless pack.
 
     Returns
     -------
-    K: float
-        Bulk modulus [GPa].
+    Kdry, Gdry : float or array_like
+        Dry rock bulk & shear modulus in GPa.
+
+    References
+    ----------
+    Mavko et al. (2009), The Rock Physics Handbook, Cambridge University Press (p.246)
     '''
-    # converts density to SI (kg/m3)
-    D = rho*1e3
-    K = D*vp**2 - 4/3*D*vs**2
-    return K/1e9
+    sigma0 = sigma / 1e3  # converts pressure in same units as solid moduli (GPa)
+    pr0 = (3*K0-2*G0) / (6*K0+2*G0)  # poisson's ratio of mineral mixture
+    Khm = (sigma0*(Cn**2*(1 - phi_c)**2*G0**2) / (
+           18*np.pi**2 * (1 - pr0)**2))**(1/3)
+    Ghm = ((2+3*f-pr0*(1+3*f)) / (5*(2-pr0))) * (
+          (sigma0 * (3 * Cn**2 * (1 - phi_c)**2 * G0**2) / (
+           2 * np.pi**2 * (1 - pr0)**2)))**(1/3)
+    return Khm, Ghm
+
+
+def softsand(K0, G0, phi, sigma, phi_c=0.4, Cn=8.6, f=1):
+    '''
+    Soft sand, or friable sand or uncemented sand model.
+    (aadm 2015)
+
+    Parameters
+    ----------
+    K0, G0 : float or array_like
+        Mineral bulk & shear modulus in GPa.
+    phi : float or array_like
+        Porosity.
+    sigma : float
+        Effective stress in MPa.
+    phi_c : float, optional
+        Critical porosity. Default: 0.4
+    Cn : float, optional
+        Coordination number Default: 8.6.
+    f : float, optional
+        Shear modulus correction factor,
+        f=1 for dry pack with perfect adhesion
+        between particles and f=0 for dry frictionless pack.
+
+    Returns
+    -------
+    Kdry, Gdry : float or array_like
+        Dry rock bulk & shear modulus in GPa.
+
+    References
+    ----------
+    Mavko et al. (2009), The Rock Physics Handbook, Cambridge University Press (p.258)
+    '''
+    Khm, Ghm = hertzmindlin(K0, G0, sigma, phi_c, Cn, f)
+    Kdry = -4/3 * Ghm + (((phi / phi_c) / (Khm + 4/3 * Ghm)) + (
+           (1 - phi / phi_c) / (K0 + 4/3 * Ghm)))**-1
+    gxx = Ghm / 6 * ((9 * Khm + 8 * Ghm) / (Khm + 2 * Ghm))
+    Gdry = -gxx + ((phi / phi_c) / (Ghm + gxx) + (
+           (1 - phi / phi_c) / (G0 + gxx)))**-1
+    return Kdry, Gdry
+
+
+
+def vels(kdry, gdry, kmin, rho0, kfluid, rhof, phi):
+    '''
+    Calculate velocities and densities of saturated rock
+    using Gassmann's equation.
+    (aadm 2015-2018)
+
+    Parameters
+    ----------
+    kdry : float or array_like
+        Dry-rock bulk modulus in GPa.
+    gdry : float or array_like
+        Dry-rock shear modulus in GPa.
+    kmin : float or array_like
+        Mineral bulk modulus in GPa.
+    rho0 : float or array_like
+        Mineral density in g/cm3.
+    kfluid : float or array_like
+        Fluid bulk modulus in GPa.
+    rhof : float or array_like
+        Fluid density in g/cm3.
+    phi : float or array_like
+        Porosity in fraction.
+
+    Returns
+    -------
+    vp, vs : float or array_like
+        P-wave and S-wave velocity in m/s.
+    rho : float or array_like
+        Density in g/cm3.
+    ksat : float or array_like
+        Saturated rock bulk modulus in GPa.
+    '''
+    # convert density to kg/m3 and elastic moduli to Pa
+    d0 = rho0*1e3
+    df = rhof*1e3
+    kd = kdry*1e9
+    gd = gdry*1e9
+    kf = kfluid*1e9
+    k0 = kmin*1e9
+    rho = d0 * (1 - phi) + df * phi
+    with np.errstate(divide='ignore', invalid='ignore'):
+        ksat = kd + (1 - kd/k0)**2 / (phi/kf + (1-phi)/k0 - kd/k0**2)
+        vp = np.sqrt((ksat+4/3*gd)/rho)
+        vs = np.sqrt(gd/rho)
+    return vp, vs, rho/1e3, ksat/1e9
 
 
 def shuey(vp1, vs1, rho1, vp2, vs2, rho2, theta, approx=True, terms=False):
@@ -137,19 +285,9 @@ def shuey(vp1, vs1, rho1, vp2, vs2, rho2, theta, approx=True, terms=False):
         return R, R0, G
     else:
         return R
-    
-    
-def gassmann_phi(ksat1, ksat2, kf1, kf2, kmin):
-    a = (kmin-ksat1)*(kmin-ksat2)*(kf1-kf2) 
-    b = (kmin-kf1)*(kmin-kf2)*(ksat1-ksat2)
-    return a / b
 
 
-def percdiff(start, end):
-    return (end-start)/start
-
-
-def classref(near=5, far=30, above=None, below=None, mx=.6, plot_brine=True):
+def classref(near=5, far=30, mx=.6, plot_brine=False, plot_colorzones=True):
     tmp_shl = np.array([[3094, 1515, 2.40, 0],
                         [2643, 1167, 2.29, 0],
                         [2192, 818, 2.16, 0],
@@ -162,7 +300,7 @@ def classref(near=5, far=30, above=None, below=None, mx=.6, plot_brine=True):
                         [3048, 1595, 2.23, .25],
                         [2134, 860, 2.11, .33],
                         [2590, 1060, 2.21, .156]])
-    avocl = ['CLASS-1', 'CLASS-2', 'CLASS-3', 'CLASS-4']
+    avocl = ['CLASS1', 'CLASS2', 'CLASS3', 'CLASS4']
     logs = ['VP', 'VS', 'RHO', 'PHI']
     shl = pd.DataFrame(tmp_shl, columns=logs, index=avocl)
     ssg = pd.DataFrame(tmp_ssg, columns=logs, index=avocl)
@@ -191,11 +329,6 @@ def classref(near=5, far=30, above=None, below=None, mx=.6, plot_brine=True):
         ax.plot(Ig, Gg, fillstyle='full', label=sh.name, marker=mm[i], mfc=cc[i], mec=cc[i], **mrkg)
         if plot_brine:
             ax.plot(Ib, Gb, fillstyle='none', label=None, marker=mm[i], mec=cc[i], **mrkb)
-    if above is not None:
-        vp0, vs0, d0 = above
-        vp1, vs1, d1 = below
-        Ax, Ix, Gx = shuey(vp0, vs0, d0, vp1, vs1, d1, angs, terms=True)
-        ax.plot(Ix, Gx, **mrk_sel)
     ax.set_xlabel('Intercept')
     ax.set_ylabel('Gradient')
     ax.legend()
@@ -203,5 +336,23 @@ def classref(near=5, far=30, above=None, below=None, mx=.6, plot_brine=True):
     ax.set_ylim(-mx, mx)
     ax.set_aspect('equal', 'box')
     ax.grid()
+    if plot_colorzones:
+        opt1 = dict(edgecolor='None', alpha=0.2)
+        cl1_area = patches.Rectangle((0.02, -1), .98, 1, facecolor=cc['CLASS1'], **opt1)
+        cl2_area = patches.Rectangle((-0.02, -1), .04, 2, facecolor=cc['CLASS2'], **opt1)
+        cl3_area = patches.Rectangle((-1, -1), .98, 1, facecolor=cc['CLASS3'], **opt1)
+        cl4_area = patches.Rectangle((-1, 0), .98, 1, facecolor=cc['CLASS4'], **opt1)
+        background = patches.Polygon([[-1, 1], [1, -1], [1, 1]], facecolor='w')
+        ax.add_patch(cl1_area)
+        ax.add_patch(cl2_area)
+        ax.add_patch(cl3_area)
+        ax.add_patch(cl4_area)
+        ax.add_patch(background)
+        opt2 = dict(ha='center', va='center', weight='bold', size='large')
+        ax.text(.15, -.3, 'Class 1', color=cc['CLASS1'], **opt2)
+        ax.text(0, -.25, 'Class 2/2p', color=cc['CLASS2'], **opt2)
+        ax.text(-.35, -.3, 'Class 3', color=cc['CLASS3'], **opt2)
+        ax.text(-.35, .15, 'Class 4', color=cc['CLASS4'], **opt2)
     if plot_brine:
         ax.set_title('Filled markers: gas, empty=brine')
+    return ax
